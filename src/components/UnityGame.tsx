996ee5f970d0709aca493c863a03d1fedd4b611a
Carlos Cabral <carlosfc.90@hotmail.com>
2025-06-05 14:14:53 -0300

    fix: styles fixes

--- src/components/UnityGame.tsx
+++ src/components/UnityGame.tsx
@@ -1,6 +1,14 @@
 export default function UnityGame() {
     return (
-        <div>
+        <div 
+            style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                display: 'flex',
+                height: '100%',
+                width: '100%',
+            }}
+        >
             <iframe
                 title="Unity WebGL Game"
                 src="/unity/index.html"
